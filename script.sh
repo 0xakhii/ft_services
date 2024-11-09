964b77517b8653146034e26c2ba7143d9474face
akhi <ojamal@student.1337.ma>
2024-11-09 22:28:32 +0100

    refactor WordPress setup and Nginx configuration; update cleanup script to remove specific files, enhance SSL proxy settings, and modify Dockerfile for PHP-FPM adjustments

--- script.sh
+++ script.sh
@@ -12,7 +12,7 @@ then
 		docker-compose -f srcs/docker-compose.yml up
 	elif [ $1 == "down" ]
 	then
-		rm -rf ~/data/wordpress
+		rm -rf ~/data/wordpress/*
 		docker-compose -f srcs/docker-compose.yml down
 	elif [ $1 == "re" ]
 	then
@@ -20,11 +20,11 @@ then
 		docker-compose -f srcs/docker-compose.yml up
 	elif [ $1 == "clean" ]
 	then
-		sudo rm -rf ~/data/wordpress
 		docker volume rm $(docker volume ls -q)
 		docker stop $(docker ps -a -q)
 		docker rm $(docker ps -a -q)
 		docker rmi $(docker images -aq)
+		sudo rm -rf ~/data/wordpress/*
 	fi
 else
 	echo "Invalid argument passed. Please pass either up, down or re"
--- srcs/wordpress/conf/setup.sh
+++ srcs/wordpress/conf/setup.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-# This script sets up wordpress and link it to the database
-wp core download  --allow-root
+# This script sets up WordPress and links it to the database
 
-wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWD --dbhost=mysql --allow-root
+# Check if WordPress is already set up
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+    echo "Downloading WordPress..."
+    wp core download --allow-root
 
-wp core install  --url=wordpress --title=ft_services --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWD --admin_email=$MYSQL_EMAIL --allow-root
+    echo "Creating wp-config.php..."
+    wp config create \
+        --dbname="${MYSQL_DATABASE}" \
+        --dbuser="${MYSQL_USER}" \
+        --dbpass="${MYSQL_PASSWD}" \
+        --dbhost="mysql" \
+        --allow-root
 
-tail -f /dev/null
+    echo "Installing WordPress..."
+    wp core install \
+        --url="https://localhost/wordpress" \
+        --title="ft_services" \
+        --admin_user="${MYSQL_USER}" \
+        --admin_password="${MYSQL_PASSWD}" \
+        --admin_email="${MYSQL_EMAIL}" \
+        --allow-root
+
+    # Update PHP-FPM configuration
+    sed -i "s/listen = 127.0.0.1:9000/listen = 9000/" /etc/php7/php-fpm.d/www.conf
+fi
+
+# Start PHP-FPM
+echo "Starting PHP-FPM..."
+php-fpm7 -F
