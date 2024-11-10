c7329d682cce0e46a5e5783bcf7b7442a72afe82
akhi <ojamal@student.1337.ma>
2024-11-10 20:11:15 +0100

    refactor Docker and Nginx configurations; enhance WordPress setup with improved user permissions, SSL handling, and PHP-FPM adjustments

--- srcs/wordpress/conf/setup.sh
+++ srcs/wordpress/conf/setup.sh
@@ -5,7 +5,7 @@
 # Check if WordPress is already set up
 if [ ! -f "/var/www/html/wp-config.php" ]; then
     echo "Downloading WordPress..."
-    wp core download --allow-root
+    wp core download --path=/var/www/html --allow-root
 
     echo "Creating wp-config.php..."
     wp config create \
@@ -23,9 +23,21 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
         --admin_password="${MYSQL_PASSWD}" \
         --admin_email="${MYSQL_EMAIL}" \
         --allow-root
-
+    # Create nginx user and group if not exists
+    if ! id -u nginx >/dev/null 2>&1; then
+        addgroup -S nginx && adduser -S -G nginx nginx
+    fi
     # Update PHP-FPM configuration
-    sed -i "s/listen = 127.0.0.1:9000/listen = 9000/" /etc/php7/php-fpm.d/www.conf
+    sed -i "s/listen = 127.0.0.1:9000/listen = 0.0.0.0:9000/" /etc/php7/php-fpm.d/www.conf
+    sed -i "s/;clear_env = no/clear_env = no/" /etc/php7/php-fpm.d/www.conf
+    sed -i "s/group = nobody/group = nginx/" /etc/php7/php-fpm.d/www.conf
+    sed -i "s/user = nobody/user = nginx/" /etc/php7/php-fpm.d/www.conf
+    sed -i "s/;listen.owner = nobody/listen.owner = nginx/" /etc/php7/php-fpm.d/www.conf
+    sed -i "s/;listen.group = nobody/listen.group = nginx/" /etc/php7/php-fpm.d/www.conf
+    sed -i "/stop editing/i if (isset(\$_SERVER['HTTP_X_FORWARDED_PROTO']) && \$_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {\n    \$_SERVER['HTTPS'] = 'on';\n}" /var/www/html/wp-config.php
+    # wp option update siteurl 'https://localhost/wordpress' --allow-root
+    # wp option update home 'https://localhost/wordpress' --allow-root
+
 fi
 
 # Start PHP-FPM
