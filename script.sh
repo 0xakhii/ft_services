1acd02e589d5d089327029e20e0a49615655808a
0xakhi <ojamal@student.1337.ma>
2025-04-24 14:32:40 +0100

    fix: everything being deployed on a k8s cluster

--- script.sh
+++ script.sh
@@ -1,8 +1,50 @@
 #!/bin/bash
+# start a minikube cluster
+minikube start --driver=virtualbox
 
-docker build -t nginx-reverse:latest -f ./srcs/nginx/Dockerfile --build-arg COPY_PATH=srcs/nginx .
-docker build -t mysql:latest -f ./srcs/mysql/Dockerfile --build-arg COPY_PATH=srcs/mysql .
-docker build -t phpmyadmin-nginx:latest -f ./srcs/phpmyadmin/nginx/Dockerfile --build-arg COPY_PATH=srcs/phpmyadmin/nginx .
-docker build -t phpmyadmin:latest -f ./srcs/phpmyadmin/Dockerfile --build-arg COPY_PATH=srcs/phpmyadmin/ .
-docker build -t wordpress-nginx:latest -f ./srcs/wordpress/nginx/Dockerfile --build-arg COPY_PATH=srcs/wordpress/nginx .
-docker build -t wordpress:latest -f ./srcs/wordpress/Dockerfile --build-arg COPY_PATH=srcs/wordpress/ .
+
+#Build docker images
+docker build -t my-mysql:latest ./srcs/mysql
+docker build -t my-nginx:latest ./srcs/nginx
+docker build -t my-wordpress:latest ./srcs/wordpress
+docker build -t my-wordpress-nginx:latest ./srcs/wordpress/nginx
+docker build -t my-phpmyadmin:latest ./srcs/phpmyadmin
+docker build -t my-phpmyadmin-nginx:latest ./srcs/phpmyadmin/nginx
+docker build -t my-ftps:latest ./srcs/ftps
+docker build -t my-influxdb:latest ./srcs/influxdb
+docker build -t my-grafana:latest ./srcs/grafana
+
+
+#Save docker images as .tar format
+docker save -o my-mysql.tar my-mysql:latest
+docker save -o my-nginx.tar my-nginx:latest
+docker save -o my-wordpress.tar my-wordpress:latest
+docker save -o my-wordpress-nginx.tar my-wordpress-nginx:latest
+docker save -o my-phpmyadmin.tar my-phpmyadmin:latest
+docker save -o my-phpmyadmin-nginx.tar my-phpmyadmin-nginx:latest
+docker save -o my-ftps.tar my-ftps:latest
+docker save -o my-influxdb.tar my-influxdb:latest
+docker save -o my-grafana.tar my-grafana:latest
+
+
+#Load docker images to minikube
+minikube image load my-mysql.tar
+minikube image load my-nginx.tar
+minikube image load my-wordpress.tar
+minikube image load my-wordpress-nginx.tar
+minikube image load my-phpmyadmin.tar
+minikube image load my-phpmyadmin-nginx.tar
+minikube image load my-ftps.tar
+minikube image load my-influxdb.tar
+minikube image load my-grafana.tar
+
+
+#Deploy
+kubectl apply -f ./srcs/metallb/metallb-config.yaml
+kubectl apply -f ./srcs/mysql/deployment.yaml
+kubectl apply -f ./srcs/nginx/deployment.yaml
+kubectl apply -f ./srcs/wordpress/deployment.yaml
+kubectl apply -f ./srcs/phpmyadmin/deployment.yaml
+kubectl apply -f ./srcs/ftps/kubernetes.yaml
+kubectl apply -f ./srcs/influxdb/kubernetes.yaml
+kubectl apply -f ./srcs/grafana/kubernetes.yaml
